03c9852ca1b688a30a83a038f0802189d40e5b58
Lex Mullin <alemulli@gmail.com>
2022-10-31 10:14:14 -0400

    worked on navbar component with log-in and register buttons/popups

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import Popup from "reactjs-popup";
+import 'reactjs-popup/dist/index.css';
 
 const Navbar = () => {
   return (
     <div id="navbar">
-        <h2>I am navbar</h2>
+        <h2>JuiceBox</h2>
+        <form id="searchBar">
+            <input type="search"
+            placeholder="Search Posts"  
+            /*onChange={handleSearch}*/ />
+        </form>
+        <Popup trigger={<button>Log In</button>} postion="bottom center">
+          <form className="submissionForm" /*onSubmit={handleSubmit}*/>
+            <h3>Log In</h3>
+            <span>
+            <label htmlFor="username">Username: </label>
+            <input id="username" type='text' required />
+            </span>
+            <span>
+            <label htmlFor="password">Password: </label>
+            <input id="password" type='password' required />
+            </span>
+            <button className="submitButton" type="submit"> Submit </button>
+          </form>
+        </Popup>
+        <Popup trigger={<button>Sign Up</button>} position="bottom center">
+          <form className="submissionForm" /*onSubmit={handleSubmit}*/>
+            <h3>Register an Account</h3>
+            <span>
+            <label htmlFor="username">Username: </label>
+            <input id="username" type='text' required />
+            </span>
+            <span>
+            <label htmlFor="password">Password: </label>
+            <input id="password" type='password' required />
+            </span>
+            <span>
+            <label htmlFor="Confirm password">Confirm Password: </label>
+            <input id="Confirm password" type='password' required />
+            </span>
+            <button className="submitButton" type="submit"> Submit </button>
+          </form>
+        </Popup>
   </div>
   );
 };
